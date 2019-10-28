fa37f051b9d1f3a6f77020ddc42b8c0e15f59a94
Андрій Олейнич <oleinitch@gmail.com>
2019-10-28 17:06:10 +0200

    commit

---
+++ Complex/Complex.cpp
@@ -0,0 +1,22 @@
+﻿// Complex.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "Complex.h"
+#include <iomanip>
+#include <string.h>
+#include <string>
+int main()
+{
+	MyComplex a(1,-1), b(0,0), c, d;
+	cin >> c;
+	cin >> d;
+	double q = 5;
+	b = a;
+	cout << b << endl;
+	cout << "c+d" << *&(c + d) << endl;
+	cout << "c-q" << *&(c-q) << endl;
+	cout << "c-d" << *&(c - d) << endl;
+	cout << "c*d" << *&(d*c) << endl;
+}
+
---
+++ Complex/Complex.h
@@ -0,0 +1,115 @@
+#pragma once
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+class MyComplex
+{
+private: 
+	double Re;
+	double Im;
+public:
+	MyComplex(double r = 0, double i = 0) { Re = r; Im = i; };
+
+	//get
+	double GetRe() {
+		return Re;
+	};
+	double GetIm() {
+		return Im;
+	};
+	//set
+	double SetRe(double val) {
+		Re = val;
+		return 1;
+	};
+	double SetIm(double val) {
+		Im = val;
+		return 1;
+	};
+	//operators
+	MyComplex& operator=(MyComplex &c) {
+		Re = c.GetRe();
+		Im = c.GetIm();
+		return (*this);
+	}
+	friend MyComplex operator+(MyComplex &a, MyComplex &b) {
+		MyComplex c;
+		c.SetRe(a.GetRe() + b.GetRe());
+		c.SetIm(a.GetIm() + b.GetIm());
+		return c;
+	}
+	friend MyComplex operator+(MyComplex a, double  b) {
+		MyComplex c;
+		c.SetRe(a.GetRe() + b);
+		c.SetIm(a.GetIm());
+		return c;
+	}
+	friend MyComplex operator+(double  b, MyComplex a) {
+		MyComplex c;
+		c.SetRe(b + a.GetRe());
+		c.SetIm(a.GetIm());
+		return c;
+	}
+	friend MyComplex operator-(MyComplex &a, MyComplex &b) {
+		MyComplex c;
+		c.SetRe(a.GetRe() - b.GetRe());
+		c.SetIm(a.GetIm() - b.GetIm());
+		return c;
+	}
+	friend MyComplex operator-(MyComplex a, double  b) {
+		MyComplex c;
+		c.SetRe(a.GetRe() - b);
+		c.SetIm(a.GetIm());
+		return c;
+	}
+	friend MyComplex operator-(double  b, MyComplex a) {
+		MyComplex c;
+		c.SetRe(b - a.GetRe());
+		c.SetIm((-1)*a.GetIm());
+		return c;
+	}
+	friend MyComplex operator*(MyComplex &a, MyComplex &b) {
+		MyComplex c;
+		c.SetRe((a.GetRe() * b.GetRe()) - (a.GetIm() * b.GetIm()));
+		c.SetIm((a.GetRe() * b.GetIm()) + (a.GetIm() * b.GetRe()));
+		return c;
+	}
+	friend MyComplex operator*(MyComplex a, double  b) {
+		MyComplex c;
+		c.SetRe(a.GetRe() * b);
+		c.SetIm(a.GetIm() * b);
+		return c;
+	}
+	friend MyComplex operator*(double  b, MyComplex a) {
+		MyComplex c;
+		c.SetRe(b * a.GetRe());
+		c.SetIm(b * a.GetIm());
+		return c;
+	}
+	// cin, cout
+	friend ostream& operator<<(ostream &stream, MyComplex &c)
+	{
+		stream << std::defaultfloat << "(" << c.GetRe() << (c.GetIm() > 0 ? "+" : "") << c.GetIm() << "i)";
+		return stream;
+	}
+	friend istream& operator>>(istream &stream, MyComplex &c)
+	{
+		double a, b;
+		stream >> a >> b;
+		c.SetRe(a);
+		c.SetIm(b);
+		return stream;
+	}
+
+	~MyComplex() {
+		//cout << "Destructor";
+	}
+
+};
+
+
